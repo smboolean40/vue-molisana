cb732d7ad3fe30ad0a52061da00b7481a02a6c0a
Kio-dev-it <pas.petito@gmail.com>
2021-09-30 12:20:14 +0200

    Refact style

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -2,11 +2,18 @@
 	<section>
 		<!-- TODO: Pasquale -->
 		<h2>Lista Prodotti</h2>
-		<div class="products">
+		<div class="products container">
 			<div class="product">
 				<img class="product__image" src="https://www.lamolisana.it/wp-content/uploads/2021/04/1-spaghetto-quadrato.jpg" alt="">
 				<div class="product__name">
-					Nome Pasta
+					N.4 Spaghetto Quadrato Bucato
+				</div>
+
+			</div>
+			<div class="product">
+				<img class="product__image" src="https://www.lamolisana.it/wp-content/uploads/2021/04/1-spaghetto-quadrato.jpg" alt="">
+				<div class="product__name">
+					N.4 Spaghetto Quadrato Bucato
 				</div>
 
 			</div>
@@ -22,25 +29,32 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../assets/style/variables.scss";
+
+
 	section {
 		background-image: url("../assets/images/bg-prods.jpg");
+		padding-top: 60px;
+		color: $mainColor;
 
 		h2{
 			text-align: center;
+			margin-bottom: 40px;
 		}
 
 		.products{
 			display: flex;
 			flex-wrap: wrap;
-			width: 80%;
-			margin: 0 auto;
 
 			.product{
 				width: 25%;
-				border: 1px solid black;
+				margin: 2px;
+				background-color: #fff;
+				padding: 25px 0;
 
 				.product__name{
 					text-align: center;
+					font-weight: 500;
 				}
 
 				.product__image{
