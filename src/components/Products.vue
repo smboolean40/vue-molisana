992fbf0511a86b7a0afd2d3f0a1bf11e7430f79b
Kio-dev-it <pas.petito@gmail.com>
2021-09-30 11:55:47 +0200

    Add product scaffolding structure

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,7 +1,17 @@
 <template>
 	<section>
 		<!-- TODO: Pasquale -->
-		<h2>Tutti i prodotti</h2>
+		<h2>Lista Prodotti</h2>
+		<div class="products">
+			<div class="product">
+				<img src="https://picsum.photos/200/300" alt="">
+				<div class="product__name">
+					Nome Pasta
+				</div>
+
+			</div>
+
+		</div>
 	</section>
 </template>
 
@@ -14,5 +24,20 @@ export default {
 <style lang="scss" scoped>
 	section {
 		background-image: url("../assets/images/bg-prods.jpg");
+
+		h2{
+			text-align: center;
+		}
+
+		.products{
+			display: flex;
+			flex-wrap: wrap;
+			width: 80%;
+			margin: 0 auto;
+
+			.product{
+				width: 25%;
+			}
+		}
 	}
 </style>
