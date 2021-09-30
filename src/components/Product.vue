529b7e34bed92b087d7315a78bca7329dde7b389
SM Boolean <samuelemadrigali1991@gmail.com>
2021-09-30 15:09:58 +0200

    Aggiunto componente Prodotto

---
+++ src/components/Product.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="product">
+		<img class="product__image" :src="prod.src" :alt="prod.titolo">
+		<div class="product__name">
+			{{prod.titolo}}
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Product",
+	props: ['prod']
+}
+</script>
+
+<style lang="scss" scoped>
+	.product{
+		width: 25%;
+		border: 2px solid #dbe9ff;
+		background-color: #fff;
+		padding: 25px 0;
+
+		.product__name{
+			text-align: center;
+			font-weight: 500;
+			width: 60%;
+			margin: 0 auto;
+		}
+
+		.product__image{
+			width: 100%;
+		}
+	}
+</style>
--- src/components/Products.vue
+++ src/components/Products.vue
@@ -3,50 +3,97 @@
 		<!-- TODO: Pasquale -->
 		<h2>Lista Prodotti</h2>
 		<div class="products container">
-			<div class="product">
-				<img class="product__image" src="https://www.lamolisana.it/wp-content/uploads/2021/04/1-spaghetto-quadrato.jpg" alt="">
-				<div class="product__name">
-					N.4 Spaghetto Quadrato Bucato
-				</div>
-
-			</div>
-			<div class="product">
-				<img class="product__image" src="https://www.lamolisana.it/wp-content/uploads/2021/04/1-spaghetto-quadrato.jpg" alt="">
-				<div class="product__name">
-					N.4 Spaghetto Quadrato Bucato
-				</div>
-
-			</div>
-			<div class="product">
-				<img class="product__image" src="https://www.lamolisana.it/wp-content/uploads/2021/04/1-spaghetto-quadrato.jpg" alt="">
-				<div class="product__name">
-					N.4 Spaghetto Quadrato Bucato
-				</div>
-
-			</div>
-			<div class="product">
-				<img class="product__image" src="https://www.lamolisana.it/wp-content/uploads/2021/04/1-spaghetto-quadrato.jpg" alt="">
-				<div class="product__name">
-					N.4 Spaghetto Quadrato Bucato
-				</div>
-
-			</div>
-			<div class="product">
-				<img class="product__image" src="https://www.lamolisana.it/wp-content/uploads/2021/04/1-spaghetto-quadrato.jpg" alt="">
-				<div class="product__name">
-					N.4 Spaghetto Quadrato Bucato
-				</div>
-
-			</div>
-
-
+			<Product v-for="(product,index) in products" :key="index" :prod="product"/>
 		</div>
 	</section>
 </template>
 
 <script>
+import Product from '@/components/Product.vue';
+
 export default {
-	name: "Products"
+	name: "Products",
+	components: {
+		Product
+	},
+	data() {
+		return {
+			products: [
+              {
+                src: "https://www.lamolisana.it/wp-content/uploads/2021/04/1-spaghetto-quadrato.jpg",
+                titolo: "N.4 Spaghetto Quadrato Bucato",
+                tipo: "lunga",
+                cottura: 6,
+              },
+              {
+                src: "https://www.lamolisana.it/wp-content/uploads/2021/04/5-fettuccine.jpg",
+                titolo: "N.5 Fettuccine",
+                tipo: "lunga",
+                cottura: 13,
+              },
+              {
+                src: "https://www.lamolisana.it/wp-content/uploads/2021/04/6-linguine.jpg",
+                titolo: "N.6 Linguine",
+                tipo: "lunga",
+                cottura: 12,
+              },
+              {
+                src: "https://www.lamolisana.it/wp-content/uploads/2021/04/7-mezze-linguine.jpg",
+                titolo: "N.7 Mezze Linguine",
+                tipo: "lunga",
+                cottura: 11,
+              },
+              {
+                src: "https://www.lamolisana.it/wp-content/uploads/2021/04/11-spaghettino-quadrato.jpg",
+                titolo: "n.11 Spaghettino Quadrato",
+                tipo: "lunga",
+                cottura: 9,
+              },
+              {
+                src: "https://www.lamolisana.it/wp-content/uploads/2021/05/38-mezze-maniche.jpg",
+                titolo: "N.38 Mezze maniche",
+                tipo: "corta",
+                cottura: 12,
+              },
+              {
+                src: "https://www.lamolisana.it/wp-content/uploads/2021/05/44-sedani.jpg",
+                titolo: "N.44 Sedani",
+                tipo: "corta",
+                cottura: 8,
+              },
+              {
+                src: "https://www.lamolisana.it/wp-content/uploads/2021/05/49-sedani.jpg",
+                titolo: "N.49 Sedanini",
+                tipo: "corta",
+                cottura: 8,
+              },
+              {
+                src: "https://www.lamolisana.it/wp-content/uploads/2021/05/53-lumachine.jpg",
+                titolo: "N.53 Lumachine",
+                tipo: "corta",
+                cottura: 12,
+              },
+              {
+                src: "https://www.lamolisana.it/wp-content/uploads/2021/05/55-chifferi-rigati.jpg",
+                titolo: "N.55 Chifferi rigati",
+                tipo: "corta",
+                cottura: 12,
+              },
+              {
+                src: "https://www.lamolisana.it/wp-content/uploads/2021/05/58-misto-corto.jpg",
+                titolo: "N.58 Misto corto",
+                tipo: "corta",
+                cottura: 9,
+              },
+              {
+                src: "https://www.lamolisana.it/wp-content/uploads/2021/06/84-ditale-quadrato.jpg",
+                titolo: "N.84 Ditale Quadrato",
+                tipo: "cortissima",
+                cottura: 12,
+              },
+            ],
+		}
+	}
 }
 </script>
 
@@ -67,24 +114,6 @@ export default {
 		.products{
 			display: flex;
 			flex-wrap: wrap;
-
-			.product{
-				width: 25%;
-				border: 2px solid #dbe9ff;
-				background-color: #fff;
-				padding: 25px 0;
-
-				.product__name{
-					text-align: center;
-					font-weight: 500;
-					width: 60%;
-					margin: 0 auto;
-				}
-
-				.product__image{
-					width: 100%;
-				}
-			}
 		}
 	}
 </style>
